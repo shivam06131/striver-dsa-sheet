159b9768bce5867fe9799d9e748de46a7943d28b
shivam06131 <sv06131@gamilcom>
2021-11-02 22:32:41 +0530

    Unique Paths most optimized solution using permutation

---
+++ Question17/method4.java
@@ -0,0 +1,19 @@
+package Question17;
+
+public class method4 {
+    public static void main(String[] args) {
+        int val2 = solution(2, 3);
+        int val3 = solution(3, 7);
+        System.out.println("val: " + val2);
+    }
+
+    static int solution(int m, int n) {
+        int N = m + n - 2;
+        int R = m - 1;
+        double sol = 1;
+        for (int i = 1; i <= R; i++) {
+            sol *= (N + i - R) / i;
+        }
+        return (int) sol;
+    }
+}
