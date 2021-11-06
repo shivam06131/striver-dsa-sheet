730cbdd3d059e0926b67e58f9e6a3caae272b2ae
shivam06131 <sv06131@gamilcom>
2021-11-06 23:43:11 +0530

    unique path (permutation solution added)

--- Question17/method4.java
+++ Question17/method4.java
@@ -2,7 +2,7 @@ package Question17;
 
 public class method4 {
     public static void main(String[] args) {
-        int val2 = solution(2, 3);
+        int val2 = solution(3, 2);
         int val3 = solution(3, 7);
         System.out.println("val: " + val2);
     }
@@ -12,7 +12,7 @@ public class method4 {
         int R = m - 1;
         double sol = 1;
         for (int i = 1; i <= R; i++) {
-            sol *= (N + i - R) / i;
+            sol = sol * (N + i - R) / i;
         }
         return (int) sol;
     }
